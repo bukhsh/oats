f81375e91dd285a2e3dfd7753ff75cc3450063fd
wbukhsh <wbukhsh@gmail.com>
2018-06-13 10:24:58 +0100

    logging functionality added to oats

--- runfile.py
+++ runfile.py
@@ -10,22 +10,43 @@
 #==================================================================
 
 from runcase import runcase
+import logging
 
-opt=({}) #pass options to the runcase script
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='oatslog.log',
+                    filemode='w')
+logging.info("OATS log file")
 
-# give complete path to the solver
-solver = 'ipopt'
+#----------------------------------------------------------------------
+def main():
+    logging.info("Program started")
+    opt=({}) #pass options to the runcase script
+    # give complete path to the solver
+    solver = 'ipopt'
+    # =====Test cases=====
+    #give a path to the testcase file under the 'testcase' folder
+    testcase = 'case24_ieee_rts.xlsx'
+    #testcase = 'IrishNetwork.xlsx'
+    # =====Model=====
+    #specify a model to solve
+    #model ='DCLF'
+    #model ='ACLF'
+    #model ='ACOPF'
+    #model ='UC'
+    model ='DCOPF'
+    #model ='SCOPF'
+    # ==log==
+    logging.info("Solver selected: "+solver)
+    logging.info("Testcase selected: "+testcase)
+    logging.info("Model selected: "+model)
+    runcase(testcase,model,solver,opt)
+    logging.info("Done!")
 
-# =====Test cases=====
-#give a path to the testcase file under the 'testcase' folder
-testcase = 'case24_ieee_rts.xlsx'
-#testcase = 'IrishNetwork.xlsx'
-# =====Model=====
-#specify a model to solve
-#model ='DCLF'
-#model ='ACLF'
-#model ='ACOPF'
-#model ='UC'
-model ='DCOPF'
-#model ='SCOPF'
-runcase(testcase,model,solver,opt)
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        logging.error("Fatal error in main loop", exc_info=True)
+        raise
