760c550b3ff0ec25d1fcdc852bdd36550803608b
Waqquas Bukhsh <waqquasbukhsh@waqquass-mbp.home>
2019-08-18 22:40:04 +0100

    docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 with open('README.md') as f:
     readme = f.read()
 setup(name='oatspower',
-      version='0.0.12',
+      version='0.0.13',
       description='OATS: Optimisation and Analysis Toolbox for Power Systems',
       long_description=readme,
       long_description_content_type='text/markdown',
@@ -11,7 +11,7 @@ setup(name='oatspower',
       author_email='wbukhsh@gmail.com',
       license='GPLv3+',
       packages=['oats','oats.models','oats.testcases'],
-      package_data={'oats.testcases': ['*'],'logo.png'},
+      package_data={'oats.testcases': ['*']},
       install_requires =[
         'pyomo',
         'tabulate',
