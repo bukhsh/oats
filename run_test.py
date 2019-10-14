80abb3a80a2d2473a527ff0462653b0c0ddefb3d
wbukhsh <wbukhsh@gmail.com>
2019-10-14 17:21:05 +0100

    some changes

---
+++ run_test.py
@@ -0,0 +1,9 @@
+# import oats
+#
+# oats.uc()
+# import os
+# pp_dir = os.path.dirname(os.path.realpath(__file__))
+#
+from oats.run import uc
+
+uc(neos=False,solver='cplex',tc='/home/waqquas/oats_testing/caseGB.xlsx')
